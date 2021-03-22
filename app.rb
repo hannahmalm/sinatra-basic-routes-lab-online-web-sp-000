eb7ba6dac2ad602b631bb0756be93107d5511ac5
Hannah Malm <hannahemalm616@gmail.com>
2021-03-22 17:48:28 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do 
+    name = "Hannah"
+    "My name is #{name}."
+  end 
+  
+  get '/hometown' do
+    hometown = "Cleveland"
+    "My hometown is #{hometown}."
+  end 
+  
+  get '/favorite-song' do 
+    favorite-song = "Hello"
+    "My favorite song is #{favorite-song}."
+  end 
+  
+  
 end
